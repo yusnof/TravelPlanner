3e4f0154cd6cdaee043104cc011798deb00276b1
Markus <meringueman2@gmail.com>
2024-05-20 14:24:24 +0200

    more shortestPath

--- Main.hs
+++ Main.hs
@@ -4,7 +4,6 @@ import Graph  -- Create a module and use a sensible graph representation
 import Data.PSQueue as PQ
 
 
-
 --type PrioQ = MinPQueue Edge (Maybe Int)
 
 --To do: 
@@ -12,23 +11,23 @@ import Data.PSQueue as PQ
 -- Make alg recursive, currently only "implemented" for the first step.
 shortestPath :: Graph a b -> a -> a -> Maybe ([a], b)
 shortestPath g from to = do -- TODO: implement Dijkstra's algorithm
-  PQ.insert from (Just 0) unvisited   -- add start
+  PQ.insert from 0 unvisited   -- add start
   
   map help adjacent -- add all adjacent (unvisited) nodes with their distance to Queue.
   
-   
    where
     recursive q g
       | PQ.null q = []
       | otherwise = do
         let current = PQ.key (PQ.findMin q) -- might be Nothing
         let adjacent = adj current g
-      
-     
+    
+    insertAll ((Edge src dst label):xs) q = insertIntoQ dst (distanceToStart (Edge src dst label)) unvisited 
+
     unvisited:: PSQ Edge (Maybe Int)
     unvisited = PQ.empty
     
-    insertIntoQ k v q= q.insert k v
+    insertIntoQ k v q= PQ.insert k v q
     distanceToStart (Edge scr dest label) = label + PQ.prio (PQ.findMin q)
 
 main :: IO ()
